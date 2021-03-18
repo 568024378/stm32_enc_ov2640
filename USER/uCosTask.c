a4e7b3739d56a56a4600c1db2c7d85a73119caa2
dajie <568024378@qq.com>
2021-03-18 21:50:04 +0800

    更改代码结构，将ucos任务转移到uCosTask.c中

--- USER/uCosTask.c
+++ USER/uCosTask.c
@@ -1,1 +1,66 @@
+#include "led.h"
+#include "delay.h"
+#include "key.h"
+#include "usart.h"	 
 #include "uCosTask.h"
+#include "includes.h"
+#include "tcp_server_demo.h"
+
+
+//LED任务
+//任务优先级
+#define LED_TASK_PRIO		10
+//任务堆栈大小
+#define LED_STK_SIZE		64
+//任务堆栈
+OS_STK	LED_TASK_STK[LED_STK_SIZE];
+//任务函数
+void led_task(void *pdata);  
+
+//START任务
+//任务优先级
+#define START_TASK_PRIO		12
+//任务堆栈大小
+#define START_STK_SIZE		128
+//任务堆栈
+OS_STK START_TASK_STK[START_STK_SIZE];
+//任务函数
+void start_task(void *pdata); 
+
+void uCos_main()
+{
+    //初始化tcp_client(创建tcp_client线程)
+    while(tcp_server_init()) 									
+	{
+		printf("TCP Server failed!!"); //tcp客户端创建失败
+		delay_ms(1000);
+	}
+
+    OSTaskCreate(start_task,(void*)0,(OS_STK*)&START_TASK_STK[START_STK_SIZE-1],START_TASK_PRIO);
+	OSStart(); //开启UCOS
+
+}
+
+
+//start任务
+void start_task(void *pdata)
+{
+	OS_CPU_SR cpu_sr;
+	pdata = pdata ;
+	
+	OSStatInit();  			//初始化统计任务
+	OS_ENTER_CRITICAL();  	//关中断
+	OSTaskCreate(led_task,(void*)0,(OS_STK*)&LED_TASK_STK[LED_STK_SIZE-1],LED_TASK_PRIO); 	//创建LED任务
+	OSTaskSuspend(OS_PRIO_SELF); //挂起start_task任务
+	OS_EXIT_CRITICAL();  //开中断
+}
+
+//led任务
+void led_task(void *pdata)
+{
+	while(1)
+	{
+		LED0 = !LED0;
+		OSTimeDlyHMSM(0,0,0,500);  //延时500ms
+ 	}
+}
---
+++ USER/uCosTask.h
@@ -0,0 +1,9 @@
+#ifndef __UCOSTASK_H
+#define __UCOSTASK_H
+
+
+extern void uCos_main(void);
+
+
+
+#endif
